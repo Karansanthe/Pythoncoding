a1f74fd76a728c234916dd230490430cfb2dd50c
Durgesh Pandey <durgeshmailac.com>
2023-02-16 16:39:15 +0530

    Commit

---
+++ slice_example.py
@@ -0,0 +1,9 @@
+word = "Hello World"
+sliced_word = word[1:4]
+print(sliced_word)
+reverse_word = word[::-1]
+print(reverse_word)
+every_other = word[::2]
+print(every_other)
+every_third = word[::3]
+print(every_third)
