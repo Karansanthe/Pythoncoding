4af987c1b92a17a189a642b060974cf45e7571b4
Durgesh Pandey <durgeshmailac@gmail.com>
2023-03-21 11:39:13 +0530

    Commit

--- rev_str_punctuation.py
+++ rev_str_punctuation.py
@@ -1,4 +1,6 @@
 import string
+
+
 def reverse_sentence_with_punctuation(input_string):
     punctuation = string.punctuation
     words = input_string.split()
@@ -10,6 +12,8 @@ def reverse_sentence_with_punctuation(input_string):
         else:
             reversed_words.append(word[::-1])
     return " ".join(reversed_words)
+
+
 input_string = "I love python programming!"
 print(reverse_sentence_with_punctuation(input_string))
 
