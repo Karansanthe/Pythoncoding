365907f02461cdafee8a63f08de95cac2c0e374e
Durgesh Pandey <durgeshmailac@gmail.com>
2023-06-23 15:47:46 +0530

    Commit

--- fib.py
+++ fib.py
@@ -1,15 +1,8 @@
-n = int(input("Enter the value of n: "))  # get the value of n from the user
-fib = [0, 1]  # initialize the first two terms of the sequence
+n_terms = int(input("Enter the number of Fibonacci terms to generate: "))
 
-if n < 2:
-    print(*fib[:n], sep=', ')  # print the first n terms of the sequence
-else:
-    for i in range(2, n):
-        next_fib = fib[i - 1] + fib[i - 2]  # calculate the next term in the sequence
-        fib.append(next_fib)  # add the next term to the list
-    print(*fib, sep=', ')  # print the entire sequence
+fib_seq = [0, 1]
+while len(fib_seq) < n_terms:
+    fib_seq.append(fib_seq[-1] + fib_seq[-2])
 
-"""
-The * is used to unpack the fib list and print its contents as separate arguments to the print() function.
-In other words, the * is used to "unpack" the list and pass its elements as separate arguments to the function.
-"""
+print("Fibonacci Sequence:")
+print(fib_seq)
