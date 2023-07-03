ae7f2dfadaeb19f8824e441a2129f40d320c3870
Durgesh Pandey <durgeshmailac@gmail.com>
2023-07-03 15:32:58 +0530

    Commit

--- max_product.py
+++ max_product.py
@@ -1,12 +1,10 @@
 numbers = [1, 2, 3, 4, 5, -9, -9]
 
-# Initialize the maximum and minimum numbers
-max1 = float('-inf')  # Initialize first maximum as negative infinity
-max2 = float('-inf')  # Initialize second maximum as negative infinity
-min1 = float('inf')   # Initialize first minimum as positive infinity
-min2 = float('inf')   # Initialize second minimum as positive infinity
+max1 = float('-inf')  
+max2 = float('-inf')
+min1 = float('inf')
+min2 = float('inf')
 
-# Find the two maximum numbers and two minimum numbers
 for num in numbers:
     if num > max1:
         max2 = max1
@@ -20,10 +18,9 @@ for num in numbers:
     elif num < min2:
         min2 = num
 
-# Compare the product of the two maximums and the two minimums
 if max1 * max2 > min1 * min2:
     result = max1 * max2
 else:
     result = min1 * min2
 
-print(result)  # Output: 81 (9 * 9 = 81)
+print(result)
