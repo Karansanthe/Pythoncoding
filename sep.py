5cade5529d84cc6d0c7375c646167a3fa47555df
Durgesh Pandey <durgeshmailac@gmail.com>
2023-07-13 12:00:52 +0530

    Commit

---
+++ sep.py
@@ -0,0 +1,16 @@
+# end= " "
+
+print("Hello", end="!!!")
+# Output: Hello!!!
+
+print("Hello", end="")
+print("World")
+# Output: HelloWorld (without newline in between)
+
+# sep= " "
+
+print("apple", "banana", "orange", sep=", ")
+# Output: apple, banana, orange
+
+print("1", "2", "3", sep="-")
+# Output: 1-2-3
