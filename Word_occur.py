d7110e63a93bb70be9f3570aee0867b842d97988
Durgesh Pandey <durgeshmailac@gmail.com>
2023-07-13 12:12:21 +0530

    Commit

--- Word_occur.py
+++ Word_occur.py
@@ -6,3 +6,13 @@ for word in words:
     words_count[word] = words_count.get(word, 0) + 1
 print(words_count)
 print(list(words_count.items()))
+
+
+# Count in array
+
+arr = [1, 3, 4, 5, 3]
+count = {}
+
+for i in arr:
+    count[i] = count.get(i, 0) + 1
+print(count)
