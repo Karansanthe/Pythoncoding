ac342685903448fc19ffc5d5734beb7ea4854538
Durgesh Pandey <durgeshmailac@gmail.com>
2023-08-18 11:38:57 +0530

    Commit

--- fib.py
+++ fib.py
@@ -4,5 +4,14 @@ fib_seq = [0, 1]
 while len(fib_seq) < n_terms:
     fib_seq.append(fib_seq[-1] + fib_seq[-2])
 
-print("Fibonacci Sequence:")
 print(fib_seq)
+
+# other method
+
+n = 5
+a, b = 0, 1
+print(a, end = ' ')
+
+for _ in range(n - 1):
+    print(b, end = ' ')
+    a, b = b, a+b
