ddb61d67d3e6d8f1609e79a7a3f5ae1107d72b2d
Durgesh Pandey <durgeshmailac@gmail.com>
2023-08-21 17:46:11 +0530

    Commit

--- peak_of_peak.py
+++ peak_of_peak.py
@@ -2,9 +2,8 @@ def find(arr):
     n = len(arr)
     peak = 0
     for i in range(1, n-1):
-        if arr[i] > arr[i-1] and arr[i] > arr[i+1]:
-            if arr[i] > peak:
-                peak = arr[i]
+        if arr[i] > arr[i-1] and arr[i] > arr[i+1] and arr[i] > peak:
+            peak = arr[i]
     return peak
 
 arr = [10, 5, 9, 6, 7, 2, 10]
