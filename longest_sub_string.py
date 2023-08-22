6e82b0d5784c1aa5fab030dc21ceadfc7f19bc32
Durgesh Pandey <durgeshmailac@gmail.com>
2023-08-22 22:24:10 +0530

    Commit

--- longest_sub_string.py
+++ longest_sub_string.py
@@ -1,12 +1,12 @@
-str1 = 'ababcdaeafab'
+s = 'ababcdaeafab'
 d1 = {}
 max_len = 0
 left = 0
 
-for right in range(len(str1)):
-    if str1[right] in d1:
-        left = max(left, d1[str1[right]] + 1)
-    d1[str1[right]] = right
+for right in range(len(s)):
+    if s[right] in d1:
+        left = max(left, d1[s[right]] + 1)
+    d1[s[right]] = right
     max_len = max(max_len, right - left + 1)
 
 print(max_len)
