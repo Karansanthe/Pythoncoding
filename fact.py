8fa47270f72b048de1616ba193525a1d345760b4
Durgesh Pandey <durgeshmailac@gmail.com>
2023-08-23 11:39:05 +0530

    Commit

--- fact.py
+++ fact.py
@@ -1,25 +1,8 @@
-num = int(input("Enter a number: "))
+num = 5
+res = 1
 
-factorial = 1
+for i in range(1, num + 1):
+    res *= i
 
-if num < 0:
-    print("Factorial does not exist for negative numbers")
-elif num == 0:
-    print("Factorial of 0 is 1")
-else:
-    for i in range(1, num + 1):
-        factorial *= i
-    print("Factorial of", num, "is", factorial)
+print(res)
 
-
-"""
-
-# find the factorial of a given number without using if-else, for, and ternary operators:
-
-num = int(input("Enter a number: "))
-result = 1
-while num:
-    result *= num
-    num -= 1
-print(result)
-"""
