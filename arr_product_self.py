3bacf6accf298888d778c232cd81dd0f9e82a8f2
Durgesh Pandey <41140945+durgeshpd@users.noreply.github.com>
2024-11-07 12:49:05 +0530

    arr_product_self.py

--- arr_product_self.py
+++ arr_product_self.py
@@ -1,11 +1,13 @@
 arr = [1, 2, 3, 4]
 res = 1
 
-for i in range(len(arr)):
-    res = res * arr[i]
+# Iterate directly over the elements of arr
+for num in arr:
+    res = res * num  # Multiply res by each element
 
 d = [] 
-for i in range(len(arr)):
-    d.append(res // arr[i])
+# Again, iterate directly over the elements of arr
+for num in arr:
+    d.append(res // num)  # Divide the product by each element and append to d
 
 print(d)
